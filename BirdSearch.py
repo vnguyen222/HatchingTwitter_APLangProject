a8f1838d46abe31ebba2e0b117460939b4a3aa7f
Vincent Nguyen <vincent.nguyennf@gmail.com>
2022-05-16 00:05:25 -0600

    Almost Done

--- BirdSearch.py
+++ BirdSearch.py
@@ -1,6 +1,6 @@
 ﻿''' HATCHING TWITTER AP LANGUAGE FINAL PROJECT '''
 ''' BY: VINCENT NGUYEN '''
-import projectfiles, searchhandling
+import backend
 
 import time
 
@@ -13,7 +13,20 @@ def CLEAR_TERMINAL():
         os.system("clear")
 # Window Sizing Handling
 def window_size():
-    size = os.get_terminal_size()
+    raw_terminal_size = str(os.get_terminal_size())
+    size_numbers = raw_terminal_size[25:]
+    size = []
+    numtostr = ""
+    for x in size_numbers:
+        if x.isdigit():
+            numtostr += x
+        else:
+            if len(numtostr) > 0:
+                size.append(int(numtostr))
+                numtostr = ""
+
+    return size
+        
 
 # Print Effect Function
 def print_effect(str, delay):
@@ -38,32 +51,38 @@ twitter_ASCII_logo = (
         "                                   ,((((((((((((((((((/.                                          ",
         "                              //(((((((((((((((((((,                                              ")
 # Special Characters Not Allowed in Search
-special_characters = "[@_!#$%^&*()<>?/\|}{~:]"
+special_characters = "[@_!#$%^&*()<>/\|}{~:]"
 
 def loading_screen():
     CLEAR_TERMINAL()
     print(platform.system(), platform.version(), platform.architecture(), "\nPython", platform.python_version(), platform.python_implementation(), platform.python_compiler(), end="\n\n")
-    projectfiles.open_files()
+    backend.open_files()
     time.sleep(1)
     CLEAR_TERMINAL()
 
 def search_screen():
     CLEAR_TERMINAL()
     print("--------------")
-    print("| BirdSearch |")
+    print("| BirdSearch |    A search for information about the creation of Twitter")
     print("--------------")
     for l in twitter_ASCII_logo:
         time.sleep(0.05)
         print(l)
+    print("#QUIT to exit")
     while True:
         # Get user search input
         search_query_preparsed = input("\nSearch: ")
+        # Exit Command
+        if search_query_preparsed == "#QUIT":
+            CLEAR_TERMINAL()
+            print("Exiting...")
+            exit()
         search_query_preparsed = search_query_preparsed.lower()
         query_valid = True
 
         # Search Checking
-        if search_query_preparsed[0] == "#" and search_query_preparsed.index(" ") < 0:  # Short search
-            return search_query_preparsed
+        if search_query_preparsed[0] == "#":  # Short search
+            return search_query_preparsed.strip()
 
         for x in search_query_preparsed:    # Checking if any digits or illegal symbols in the string
             if x.isdigit():
@@ -79,20 +98,21 @@ def search_screen():
         else:
             print ("Enter a valid search")
 
-def result_screen(user_search, requested_file):
-    CLEAR_TERMINAL()
-    print("Showing results for:", user_search)
-    
-    
 
 
 # EXECUTE MAIN
 if __name__ == "__main__":
     loading_screen()
+
+    #backend.print_result("#summary", "rawfiles/chaptersummaries.txt", window_size())
     
-    '''
     while True:
         search_query = search_screen()
-        result_file = searchhandling.parse_query(search_query)
-        result_screen(search_query, result_file)'''
+        result_file = None
+        try:
+            result_file = backend.parse_query(search_query)
+        except Exception as e:
+            print(e)
+        else:
+            backend.print_result(search_query, result_file, window_size())
     
---
+++ backend.py
@@ -0,0 +1,99 @@
+﻿# Project Files Handling
+
+import keyboard, time, os
+
+def CLEAR_TERMINAL():
+    if os.name == 'nt':
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+FILE_LOCATIONS = ("rawfiles/chaptersummaries.txt", "rawfiles/timeline.txt", "rawfiles/summary.txt", "rawfiles/reflection.txt")
+FILES = []
+
+def open_files():
+    for i in range(0, len(FILE_LOCATIONS)):
+        try:
+            FILES.append(open(FILE_LOCATIONS[i]))
+        except Exception as e:
+            print("File", FILE_LOCATIONS[i], "Import ERROR")
+            print(e)
+        else:
+            print(FILE_LOCATIONS[i], "successfully imported")
+
+def print_result(search, filename, screen_dimensions):
+    index = FILE_LOCATIONS.index(filename)
+    file = FILES[index].readlines()
+    start, end = 0, screen_dimensions[1]-15
+    while True:
+        CLEAR_TERMINAL()
+        print("Showing results for:", search)
+        for x in range(0, screen_dimensions[0]):
+            print("-", end="")
+        print()
+        
+        if len(file) < screen_dimensions[1]-15:
+            for x in file[start:]:
+                print(x, end="")
+        else:
+            for x in file[start:end]:
+                print(x, end="")
+        print()
+
+        print("--[Quit: q]--[Scroll: ↑↓]--", end="")
+        for x in range(27, screen_dimensions[0]):
+            print("-", end="")
+        print("")
+
+        while True:
+            if keyboard.is_pressed('q'):
+                return
+            if keyboard.is_pressed("up arrow"):
+                if start > 0:
+                    start -= 1
+                    end -= 1
+                    time.sleep(0.01)
+                    break
+            if keyboard.is_pressed("down arrow"): 
+                if end < len(file):
+                    start += 1
+                    end += 1
+                    time.sleep(0.01)
+                    break
+
+
+# Search Query Handling
+
+def result_file(search):
+    if "chapter" in search and "summary" in search:
+        return FILE_LOCATIONS[0]
+    elif "summary" in search:
+        return FILE_LOCATIONS[2]
+    elif "timeline" in search:
+        return FILE_LOCATIONS[1]
+    else:
+        raise Exception("Invalid Search Query")
+
+
+COMMON_QUESTION_WORDS = ("what", "where", "who", "why")
+
+def mispelled_similar(base_word, user_word):
+    if abs(len(user_word) - len(base_word)) < 2:
+        return False
+
+
+
+
+def parse_query(user_search):
+    search = None
+    if user_search[0] == "#":
+        search = [user_search[1:]]
+    else:
+        search = user_search.split()
+
+    return result_file(search)
+
+    '''
+    for q_word in COMMON_QUESTION_WORDS:
+        if search[0] == q_word or :'''
